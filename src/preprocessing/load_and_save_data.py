9a3a975fae9825862be20f33808b5f8807e8cb0a
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2024-02-28 20:57:33 +0100

    add image info in dataframe

--- src/preprocessing/load_and_save_data.py
+++ src/preprocessing/load_and_save_data.py
@@ -1,18 +1,39 @@
 import os
+from PIL import Image
 from tqdm import tqdm
 from pathlib import Path
+import numpy as np
 import pandas as pd
 
 DATAPATH = Path(__file__).resolve().parent.parent.parent / "data" / "train"
 
 
-def load_and_save_train_images(dir: str):
-    df = pd.DataFrame(columns=["path", "label"])
+def load_and_save_train_images(dir: str) -> pd.DataFrame:
+    """ "
+    Load and save images from a directory to a pandas Data Frame.
+    Args:
+        dir: str, directory where the images are stored.
+    Returns:
+        df: pd.DataFrame, a pandas Data Frame with the following columns:
+            - relative_path: str, relative path of the image.
+            - label: str, label of the image.
+            - image: list, list of pixels representing the image.
+    Warning:
+        To load the images from the image column in obtained Data Frame, use the following code:
+        `df['image'].apply(lambda x: np.array(eval(x)))`
+    """
+    df = pd.DataFrame(columns=["relative_path", "label", "image"])
     for subdir in tqdm(os.listdir(dir)):
         for file in tqdm(os.listdir(os.path.join(dir, subdir))):
             if file.endswith(".png") or file.endswith(".jpg"):
+                img = Image.open(dir / subdir / file)
+                img.info.pop("icc_profile", None)
                 new_record = pd.DataFrame(
-                    {"relative_path": [os.path.join(subdir, file)], "label": [subdir]}
+                    {
+                        "relative_path": [os.path.join(subdir, file)],
+                        "label": [subdir],
+                        "image": [np.array(img).tolist()],
+                    }
                 )
                 df = pd.concat([df, new_record])
     return df
@@ -20,7 +41,7 @@ def load_and_save_train_images(dir: str):
 
 def main():
     df = load_and_save_train_images(DATAPATH)
-    df.to_csv(DATAPATH / "train.csv", index=False)
+    df.to_csv(DATAPATH / "df_train.csv", index=False)
 
 
 if __name__ == "__main__":
