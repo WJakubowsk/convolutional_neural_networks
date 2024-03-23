59b72002ba1d9f4ad95ca66bf3f11621863dad50
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2024-03-23 13:18:50 +0100

    add softmax normalization

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -18,6 +18,7 @@ class ConvolutionalNeuralNetwork(torch.nn.Module):
         self.fc1 = torch.nn.Linear(64 * 4 * 4, 128)
         self.fc2 = torch.nn.Linear(128, out_channels)
         self.relu = torch.nn.ReLU()
+        self.softmax == torch.nn.Softmax(dim=1)
         self.maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout = torch.nn.Dropout(0.5)
 
@@ -39,6 +40,7 @@ class ConvolutionalNeuralNetwork(torch.nn.Module):
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
+        x = self.softmax(x)
         return x
 
     def fit(
