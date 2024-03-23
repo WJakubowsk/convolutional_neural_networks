6e02fc199276155502224bd460c1d71fd7389d16
WJakubowsk <72559388+WJakubowsk@users.noreply.github.com>
2024-03-23 12:13:51 +0100

    add test set preprocessing

--- src/preprocessing/load_and_save_data.py
+++ src/preprocessing/load_and_save_data.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
-DATAPATH = Path(__file__).resolve().parent.parent.parent / "data" / "train"
+DATAPATH = Path(__file__).resolve().parent.parent.parent / "data"
 
 
 def load_and_save_train_images(dir: str) -> pd.DataFrame:
@@ -39,9 +39,32 @@ def load_and_save_train_images(dir: str) -> pd.DataFrame:
     return df
 
 
+def load_and_save_test_images(dir: str) -> pd.DataFrame:
+    """ "
+    Load and save images from a directory to a pandas Data Frame.
+    Args:
+        dir: str, directory where the test images are stored.
+    """
+    df = pd.DataFrame(columns=["relative_path", "image"])
+    for file in tqdm(os.listdir(os.path.join(dir, dir))):
+        if file.endswith(".png") or file.endswith(".jpg"):
+            img = Image.open(dir / file)
+            img.info.pop("icc_profile", None)
+            new_record = pd.DataFrame(
+                {
+                    "relative_path": [os.path.join(dir, file)],
+                    "image": [np.array(img).tolist()],
+                }
+            )
+            df = pd.concat([df, new_record])
+    return df
+
+
 def main():
-    df = load_and_save_train_images(DATAPATH)
-    df.to_csv(DATAPATH / "df_train.csv", index=False)
+    df_train = load_and_save_train_images(DATAPATH / "train")
+    df_test = load_and_save_test_images(DATAPATH / "test")
+    df_train.to_csv(DATAPATH / "df_train.csv", index=False)
+    df_test.to_csv(DATAPATH / "df_test.csv", index=False)
 
 
 if __name__ == "__main__":
